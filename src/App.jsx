b7f103824d3472c4cc73144e894fd2f48cd02d89
Santiago <santiagof00@gmail.com>
2022-10-11 01:52:04 -0300

    Agrego rutas de la diferentes paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css' 
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Inicio } from "./components/views/Inicio";
+import { Administrador } from "./components/views/Administrador";
+import { Error404 } from "./components/views/Error404";
 
 function App() {
   return (
-    <div >
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/administrar" element={<Administrador></Administrador>}></Route>
+        <Route exact path="*" element={<Error404></Error404>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/views/Error404.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Error404 = () => {
+  return (
+    <div><h1>pagina error</h1></div>
+  )
+}
